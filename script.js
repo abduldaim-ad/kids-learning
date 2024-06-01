f78e3f785b711f7d23f2cee10f8011d126df8f2f
Abdul Daim <gdaim551@gmail.com>
2024-06-01 17:16:27 +0500

    padding resolved

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function getRandomLetter(language) {
         let alphabet;
-        console.log(language,"djflsd")
         if (language === 'englishcaps') {
             alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         } else if (language === 'englishsmall') {
@@ -52,12 +51,13 @@ document.addEventListener("DOMContentLoaded", function() {
         let randomNum;
 
         const randomBackground = getRandomBackground();
-        numberContainer.style.backgroundImage = randomBackground;
+        numberContainer.style.backgroundImage = `url(${randomBackground})`;
+
 
         if (sequenceModeCheckbox.checked) {
             currentNumber++;
             if (currentNumber > max) currentNumber = min;
-            randomNum = currentNumber;
+            randomNum = currentNumber-1;
         } else {
             randomNum = getRandomNumber(min, max);
         }
@@ -88,7 +88,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let currentLetterIndex = 0;
 
     function getNextSequenceNumber(min, max) {
-        currentNumber++;
         if (currentNumber > max) currentNumber = min;
         return currentNumber;
     }
@@ -98,6 +97,28 @@ document.addEventListener("DOMContentLoaded", function() {
         currentLetterIndex++;
         if (currentLetterIndex >= alphabetLength) currentLetterIndex = 0;
         return getRandomLetter(language);
+    }
+
+    const backgrounds = [
+        "https://miro.medium.com/v2/resize:fit:587/1*K4ftlEr6o_vZp82WeQsOYA.png",
+        "https://i.pinimg.com/736x/56/35/8e/56358e98473449c8473d87e2136f1c7c.jpg",
+        "https://i.ytimg.com/vi/sQOnw20Bi8Q/maxresdefault.jpg",
+        'https://5.imimg.com/data5/SELLER/Default/2023/4/299928290/FD/RJ/TH/13841671/bmw-electric-car-for-baby-a-very-beautiful-500x500.PNG',
+        'https://s.alicdn.com/@sc04/kf/H651b37caf2384568a80181662ea4b383D.jpg_300x300.jpg',
+        'https://m.media-amazon.com/images/I/51xltF+1NoL._AC_UY1100_.jpg',
+        'https://cdn.firstcry.com/education/2023/01/03161719/The-Peacock-Story-With-Moral-For-Kids.jpg',
+        'https://t3.ftcdn.net/jpg/03/77/56/44/360_F_377564428_ArCq8fvWhYVabppKvYF4uDUGZn21VvRV.jpg',
+        'https://img.freepik.com/premium-vector/vector-illustration-cute-sparrow-kids-story-book_925324-11561.jpg',
+        'https://www.kidsemporium.com.pk/wp-content/uploads/2019/03/dsc_2701.jpg',
+        'https://d1iv6qgcmtzm6l.cloudfront.net/products/lg_5gVeaTjEbtWxmre6lZy1O8WkFXtf3jUAR1rf8vpt.jpg',
+        'https://t3.ftcdn.net/jpg/05/66/98/54/360_F_566985432_rkFN4MBsAnjNJjIXCKgitq0K1l4Ksz44.jpg',
+        'https://www.india.com/wp-content/uploads/2023/10/10-Most-Beautiful-Fish-Breeds-To-Keep-At-Home.png',
+        'https://static-01.daraz.pk/p/4fc6e2ea78d1580a6692bcc833027b26.jpg_750x750.jpg_.webphttps://i.ytimg.com/vi/9GqaayqAkd8/sddefault.jpg',
+    ];    
+
+    function getRandomBackground() {
+        const randomIndex = Math.floor(Math.random() * backgrounds.length);
+        return backgrounds[randomIndex];
     }
 
     numberContainer.addEventListener("click", updateNumberAndColor);
